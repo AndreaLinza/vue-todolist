c14a5a284ac71b1dbffd3a2dbedacb870ee00c0c
AndreaLinza <andrealinza@gmail.com>
2023-07-22 17:16:05 +0200

    milestone 1 done with relative bonus

--- script.js
+++ script.js
@@ -1,55 +1,84 @@
 Vue.createApp({
     data() {
         return {
+            //lastIdItem: 0,   Se è vuota la lista
+            lastIdItem:4,
             newTodoItem: {
-                id: 5,
+                id: this.lastIdItem,
                 text: "",
                 done: ""
             },
             todoList: [
                 {
-                    id:1,
+                    id: 1,
                     text: "primo-task",
                     done: true
                 },
                 {
-                    id:2,
-                    text: "primo-task",
+                    id: 2,
+                    text: "secondo-task",
                     done: false
                 },
                 {
-                    id:3,
-                    text: "primo-task",
+                    id: 3,
+                    text: "terzo-task",
                     done: true
                 },
                 {
-                    id:4,
-                    text: "primo-task",
+                    id: 4,
+                    text: "quarto-task",
                     done: false
-                    
+
                 },
-                
+
             ],
+
             
-            lastIdItem: 5,
         }
     },
-    
+
     methods: {
         clickTodoList() {
-            const itemClone = { ...this.newTodoItem }
+            const itemClone = { ...this.newTodoItem, 
+                id: ++this.lastIdItem,
+            }
+            //console.log(this.todoList.id, itemClone)
+            
+            
             this.todoList.push(itemClone)
             console.log(this.todoList)
-            ++this.newTodoItem.id
-
-            console.log(this.newTodoItem.id)
-        },
-        
-        cancelItem(item){
-            console.log(item)
-        },
-
+    },
     
+    lineThrough(todo){
 
-    }
+        if(todo.done === true){
+            todo.done = false
+        }else{
+            todo.done = true
+        }
+        
+    },
+
+    cancelItem(itemID) {
+
+        let indexItemDelete = this.todoList.findIndex((todoItem) => todoItem.id === itemID)
+        
+        //Versione forEach
+
+        // let indexItemDelete;
+
+        // this.todoList.forEach((todoItem, i) => {
+        //     const todoItems = this.todoList[i]
+
+        //     if(todoItems.id === itemID){
+        //         indexItemDelete = i
+        //     }
+        // });
+        this.todoList.splice(indexItemDelete, 1)
+        console.log(indexItemDelete)
+    },
+
+
+
+}
 }).mount("#app")
